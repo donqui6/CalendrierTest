48f66b4a1c4a1bc6ea7026ba53557f10be1a0d1d
donqui6 <119797992+donqui6@users.noreply.github.com>
2024-03-25 10:24:50 +0100

    Version 0.1
    test de la méthode : DernierJourDuMois

---
+++ DateTest1.java
@@ -0,0 +1,25 @@
+package modele;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class DateTest {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    @DisplayName("test date du lendemain")
+    void dernierJourDuMoisTest() {
+        assertEquals(28, Date.dernierJourDuMois(2,2023));
+        assertEquals(29, Date.dernierJourDuMois(2,2024));
+        assertEquals(31, Date.dernierJourDuMois(3,2023));
+        assertEquals(31, Date.dernierJourDuMois(3,2024));
+
+    }
+    
+    }
